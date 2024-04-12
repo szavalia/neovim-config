99d067c369c66c2db204bcbe7db3de554798b47b
Salustiano Zavalía <58373778+szavalia@users.noreply.github.com>
2024-04-12 18:27:21 -0300

    Tweak Harpoon

--- init.lua
+++ init.lua
@@ -284,6 +284,7 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       }
     end,
   },
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -7,40 +7,36 @@ return {
     harpoon:setup {}
 
     -- basic telescope configuration
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
+    -- local conf = require('telescope.config').values
+    -- local function toggle_telescope(harpoon_files)
+    --   local file_paths = {}
+    --   for _, item in ipairs(harpoon_files.items) do
+    --     table.insert(file_paths, item.value)
+    --   end
+    --
+    --   require('telescope.pickers')
+    --     .new({}, {
+    --       prompt_title = 'Harpoon',
+    --       finder = require('telescope.finders').new_table {
+    --         results = file_paths,
+    --       },
+    --       previewer = conf.file_previewer {},
+    --       sorter = conf.generic_sorter {},
+    --     })
+    --     :find()
+    -- end
 
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
-
-    -- vim.keymap.set('n', '<C-e>', function()
-    --   harpoon.ui:toggle_quick_menu(harpoon:list())
-    -- end)
+    -- vim.keymap.set('n', '<leader>hm', function()
+    --   toggle_telescope(harpoon:list())
+    -- end, { desc = 'Show Harpoon marks' })
 
     vim.keymap.set('n', '<leader>hm', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = 'Show Harpoon marks' })
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
 
     vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():add()
     end, { desc = 'Add file to Harpoon marks' })
-
-    vim.keymap.set('n', '<leader>hd', function()
-      harpoon:list():add()
-    end, { desc = 'Add file to Harpoon' })
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '<leader>hn', function()
@@ -50,29 +46,36 @@ return {
     vim.keymap.set('n', '<leader>hp', function()
       harpoon:list():previous()
     end, { desc = 'Navigate to previous file in list' })
-    
 
     -- Open marks by number
-    vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end, {desc = "Jump to tag 1"})
-    vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end, {desc = "Jump to tag 2"})
-    vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end, {desc = "Jump to tag 3"}) 
-    vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end, {desc = "Jump to tag 4"})
+    vim.keymap.set('n', '<leader>hh', function()
+      harpoon:list():select(1)
+    end, { desc = 'Jump to tag 1' })
+    vim.keymap.set('n', '<leader>hj', function()
+      harpoon:list():select(2)
+    end, { desc = 'Jump to tag 2' })
+    vim.keymap.set('n', '<leader>hk', function()
+      harpoon:list():select(3)
+    end, { desc = 'Jump to tag 3' })
+    vim.keymap.set('n', '<leader>hl', function()
+      harpoon:list():select(4)
+    end, { desc = 'Jump to tag 4' })
 
-  -- Extend harpoon UI
-  harpoon:extend({
-  UI_CREATE = function(cx)
-    vim.keymap.set("n", "<C-v>", function()
-      harpoon.ui:select_menu_item({ vsplit = true })
-    end, { buffer = cx.bufnr })
+    -- Extend harpoon UI
+    harpoon:extend {
+      UI_CREATE = function(cx)
+        vim.keymap.set('n', '<C-v>', function()
+          harpoon.ui:select_menu_item { vsplit = true }
+        end, { buffer = cx.bufnr })
 
-    vim.keymap.set("n", "<C-x>", function()
-      harpoon.ui:select_menu_item({ split = true })
-    end, { buffer = cx.bufnr })
+        vim.keymap.set('n', '<C-x>', function()
+          harpoon.ui:select_menu_item { split = true }
+        end, { buffer = cx.bufnr })
 
-    vim.keymap.set("n", "<C-t>", function()
-      harpoon.ui:select_menu_item({ tabedit = true })
-    end, { buffer = cx.bufnr })
-  end,
-})
+        vim.keymap.set('n', '<C-t>', function()
+          harpoon.ui:select_menu_item { tabedit = true }
+        end, { buffer = cx.bufnr })
+      end,
+    }
   end,
 }
