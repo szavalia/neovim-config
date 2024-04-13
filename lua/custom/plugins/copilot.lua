d74449fe86540a8a2ff05a77bd1156937cf1f9dd
szavalia <szavalia@itba.edu.ar>
2024-04-13 18:07:08 -0300

    Add copilot toggling keymaps

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,11 @@
 return {
   'github/copilot.vim',
   config = function()
-  end
+    -- Disable copilot for now
+    vim.g.copilot_enabled = 0
+
+    -- Set a keymap to enable and disable copilot
+    vim.api.nvim_set_keymap('i', '<C-E>', '<cmd>Copilot enable<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-D>', '<cmd>Copilot disable<CR>', { noremap = true, silent = true })
+  end,
 }
