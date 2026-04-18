42b40177ceb2506a10d3bb79c6ce8e7023e26cdb
Salustiano Zavalía <sj.zavalia@gmail.com>
2026-04-17 22:30:40 -0300

    Use native commenting
    Replace broken Comment.nvim plugin

--- init.lua
+++ init.lua
@@ -250,13 +250,6 @@ require('lazy').setup({
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
